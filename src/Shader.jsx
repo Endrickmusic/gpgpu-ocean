6bbdaf133eb5c2715f9b2f31829fffa5109c8279
Christian Hohenbild <mail@christianhohenbild.de>
2024-11-09 23:37:56 +0100

    add gpgpu logic and debug plane

--- src/Shader.jsx
+++ src/Shader.jsx
@@ -3,84 +3,82 @@ import { useRef, useEffect } from "react"
 import { DoubleSide } from "three"
 import { useControls } from "leva"
 
-import ModifiedShader from './ModifiedShader.jsx'
+import ModifiedShader from "./ModifiedShader.jsx"
+import GPGPUHeightmap from "./Gpgpu.jsx"
 
+export default function Shader() {
+  const meshRef = useRef()
+  const materialRef = useRef()
+  const debugObject = {}
 
-export default function Shader(){
+  debugObject.Color = "#4242c1"
 
-    const meshRef = useRef()
-    const materialRef = useRef()
-    const debugObject = {}
+  const options = useControls("Controls", {
+    BigElevation: { value: 0.35, min: -5, max: 5, step: 0.001 },
+    BigFrequency: { value: 3.4, min: 0, max: 30, step: 0.001 },
+    BigSpeed: { value: 0.4, min: -5, max: 5, step: 0.001 },
+    NoiseRangeDown: { value: -1.3, min: -1.3, max: 0, step: 0.001 },
+    NoiseRangeUp: { value: 1.3, min: 0, max: 1.3, step: 0.001 },
+    Wireframe: false,
+  })
 
-    debugObject.Color = '#4242c1'
+  useEffect(
+    (state, delta) => {
+      if (meshRef.current.material.userData.shader) {
+        meshRef.current.material.userData.shader.uniforms.uBigWaveElevation.value =
+          options.BigElevation
+        meshRef.current.material.userData.shader.uniforms.uBigWaveFrequency.value =
+          options.BigFrequency
+        meshRef.current.material.userData.shader.uniforms.uBigWaveSpeed.value =
+          options.BigSpeed
+        meshRef.current.material.userData.shader.uniforms.uNoiseRangeDown.value =
+          options.NoiseRangeDown
+        meshRef.current.material.userData.shader.uniforms.uNoiseRangeUp.value =
+          options.NoiseRangeUp
 
-    const options = useControls("Controls",{
-      BigElevation: { value: 0.35, min: -5, max: 5, step: 0.001 },
-      BigFrequency: { value: 3.4, min: 0, max: 30, step: 0.001 },
-      BigSpeed: { value: .4, min: -5, max: 5, step: 0.001 },
-      NoiseRangeDown: { value: -1.3, min: -1.3, max: 0, step: 0.001 },
-      NoiseRangeUp: { value: 1.3, min: 0., max: 1.3, step: 0.001 },
-      Wireframe: false
-      })
+        materialRef.current.wireframe = options.Wireframe
+      }
+    },
+    [options]
+  )
 
+  const envMap = useEnvironment({
+    files: "./environments/aerodynamics_workshop_2k.hdr",
+  })
+  const [normalMap, roughnessMap] = useTexture([
+    "./textures/waternormals.jpeg",
+    "./textures/SurfaceImperfections003_1K_var1.jpg",
+  ])
 
-      useEffect((state, delta) => {
- 
-          if (meshRef.current.material.userData.shader) {
-
-            meshRef.current.material.userData.shader.uniforms.uBigWaveElevation.value = options.BigElevation
-            meshRef.current.material.userData.shader.uniforms.uBigWaveFrequency.value = options.BigFrequency
-            meshRef.current.material.userData.shader.uniforms.uBigWaveSpeed.value = options.BigSpeed
-            meshRef.current.material.userData.shader.uniforms.uNoiseRangeDown.value = options.NoiseRangeDown
-            meshRef.current.material.userData.shader.uniforms.uNoiseRangeUp.value = options.NoiseRangeUp
-            
-            materialRef.current.wireframe = options.Wireframe
-          }
-        },
-        [options]
-
-      )
-  
-  const envMap = useEnvironment({files:'./environments/aerodynamics_workshop_2k.hdr'})
-  const [normalMap, roughnessMap] = useTexture(['./textures/waternormals.jpeg', './textures/SurfaceImperfections003_1K_var1.jpg'])
-  
   return (
     <>
-      <OrbitControls />  
+      <OrbitControls />
 
-      <directionalLight 
-      position={[0, 2, 0]}
-      intensity={3}
-      />
-      <group>      
-        <mesh 
-        ref={meshRef}
-        scale={1}
-        rotation={[Math.PI, 0, 0.2 * Math.PI]}
-        position={[-0.2, -0.15, 0]}
+      <directionalLight position={[0, 2, 0]} intensity={3} />
+      <group>
+        <mesh
+          ref={meshRef}
+          scale={1}
+          rotation={[Math.PI, 0, 0.2 * Math.PI]}
+          position={[-0.2, -0.15, 0]}
         >
-            <planeGeometry 
-            args={[2, 2, 256, 256]} 
-            />
-            <meshStandardMaterial
-              ref={materialRef}
-              side={DoubleSide}
-              wireframe={false}
-              roughness={0.05}
-              roughnessMap={roughnessMap}
-              metalness={0.3}
-              envMap={envMap}
-              normalMap={normalMap}
-              normalScale={0.05}
-            />
+          <planeGeometry args={[2, 2, 256, 256]} />
+          <meshStandardMaterial
+            ref={materialRef}
+            side={DoubleSide}
+            wireframe={false}
+            roughness={0.05}
+            roughnessMap={roughnessMap}
+            metalness={0.3}
+            envMap={envMap}
+            normalMap={normalMap}
+            normalScale={0.05}
+          />
         </mesh>
 
-        <ModifiedShader 
-        options={options}
-        meshRef={meshRef}
-        /> 
-
+        <ModifiedShader options={options} meshRef={meshRef} />
+        <GPGPUHeightmap options={options} />
       </group>
-   
-   </>
-  )}
+    </>
+  )
+}
